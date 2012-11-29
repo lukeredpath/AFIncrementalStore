ae08284a3d76c229b5349ec459eb116e144d681e
Luke Redpath <luke@lukeredpath.co.uk>
2012-11-29 16:29:26 +0000

    Move AFIncrementalStore into a project of its own.

---
+++ AFIncrementalStore/AFIncrementalStoreTests/AFIncrementalStoreTests.m
@@ -0,0 +1,32 @@
+//
+//  AFIncrementalStoreTests.m
+//  AFIncrementalStoreTests
+//
+//  Created by Luke Redpath on 29/11/2012.
+//  Copyright (c) 2012 LJR Software Ltd. All rights reserved.
+//
+
+#import "AFIncrementalStoreTests.h"
+
+@implementation AFIncrementalStoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AFIncrementalStoreTests");
+}
+
+@end
